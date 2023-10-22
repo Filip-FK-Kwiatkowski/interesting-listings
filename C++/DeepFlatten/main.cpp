e81190015eedef3092b3ef16f5e097e2be933f83
Filip-FK-Kwiatkowski <107815348+Filip-FK-Kwiatkowski@users.noreply.github.com>
2023-10-22 22:10:25 +0200

    Create main.cpp

---
+++ C++/DeepFlatten/main.cpp
@@ -0,0 +1,43 @@
+#include <ranges>
+#include <vector>
+#include <set>
+#include <utility>
+
+#include <fmt/ranges.h>
+
+#define FWD(...) ::std::forward<decltype(__VA_ARGS__)>(__VA_ARGS__)
+
+#define LIFT(x) [](auto&&... args)  \
+noexcept(noexcept(x(FWD(args)...))) \
+-> decltype(x(FWD(args)...))        \
+{                                   \
+    return x(FWD(args)...);         \
+}
+
+auto deepFlatten(std::ranges::range auto&& rng) -> auto {
+    using rangeElement = std::ranges::range_value_t<decltype(rng)>;
+    if constexpr(std::ranges::range<rangeElement>) {
+        return rng
+                | std::views::transform(LIFT(deepFlatten))
+                | std::views::join;
+    } else {
+        return std::views::all(rng);
+    }
+}
+
+auto main() -> int {
+    auto flat = std::vector<int>{1, 2, 3};
+
+    auto deep = std::vector<std::set<int>>{
+            {1, 2}, {5, 3, 4}, {6}
+    };
+
+    auto deeper = std::set<std::vector<std::set<int>>>{
+            {{1, 2}, {3}, {4, 5, 6}},
+            {{7}, {8, 9, 10}}
+    };
+
+    fmt::println("{}", deepFlatten(flat));
+    fmt::println("{}", deepFlatten(deep));
+    fmt::println("{}", deepFlatten(deeper));
+}
