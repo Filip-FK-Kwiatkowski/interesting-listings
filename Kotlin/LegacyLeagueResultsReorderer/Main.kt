509e34017edb64ee14ea3ae1e868678d7ea6a0e6
Filip-FK-Kwiatkowski <107815348+Filip-FK-Kwiatkowski@users.noreply.github.com>
2025-04-24 17:16:26 +0200

    Create Main.kt

---
+++ Kotlin/LegacyLeagueResultsReorderer/Main.kt
@@ -0,0 +1,53 @@
+package org.example
+
+import java.io.File
+
+class PlayerRow(
+    val nameAndSurname: String,
+    var totalPoints: Int,
+    val points: MutableList<Int?> = mutableListOf(),
+): Comparable<PlayerRow> {
+    fun recalculateTotalPoints() {
+        totalPoints = points.filterNotNull().sum()
+    }
+
+    override fun compareTo(other: PlayerRow): Int {
+        return other.totalPoints.compareTo(totalPoints)
+    }
+
+    override fun toString(): String {
+        val formattedPoints = points.joinToString(separator = " | ") { it?.toString().orEmpty() }
+
+        return "| $nameAndSurname | $$ $totalPoints $$ | $formattedPoints |"
+    }
+
+    companion object {
+        fun fromLine(line: String): PlayerRow {
+            val rawPoints = line.split("|").drop(4).dropLast(1)
+
+            val nameAndSurname = line.split("|")[2].trim()
+            val points = rawPoints.map { it.trim().toIntOrNull() }
+
+            val result = PlayerRow(nameAndSurname, 0, points.toMutableList())
+            result.recalculateTotalPoints()
+            return result
+        }
+    }
+}
+
+fun main() {
+    val path ="path/to/your/file.md"
+    val playerRows = File(path).useLines { lines ->
+        lines
+            .filter{ it.startsWith('|') }
+            .drop(2)
+            .map(PlayerRow::fromLine)
+            .toMutableList()
+    }
+
+    playerRows.sort()
+
+    for ((index, player) in playerRows.withIndex()) {
+        println("| $$ ${index + 1}. $$ $player")
+    }
+}
