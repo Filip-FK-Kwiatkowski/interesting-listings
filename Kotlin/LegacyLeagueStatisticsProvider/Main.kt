7bb14c3ca74ab52d5bffc7b41501a30dca59d0ef
Fureeish <37291264+Fureeish@users.noreply.github.com>
2025-06-12 22:37:51 +0200

    Update Main.kt

--- Kotlin/LegacyLeagueStatisticsProvider/Main.kt
+++ Kotlin/LegacyLeagueStatisticsProvider/Main.kt
@@ -2,35 +2,6 @@ package org.example
 
 import java.io.File
 
-class PlayerRecord(
-    private val nameAndSurname: String,
-    private val scores: List<Int?> = listOf(),
-) : Comparable<PlayerRecord> {
-    private val totalPoints: Int = scores.filterNotNull().sum()
-
-    override fun compareTo(other: PlayerRecord): Int {
-        return other.totalPoints.compareTo(totalPoints)
-    }
-
-    override fun toString(): String {
-        val formattedPoints = scores.joinToString(separator = " | ") { it?.toString().orEmpty() }
-
-        return "| $nameAndSurname | $$ $totalPoints $$ | $formattedPoints |"
-    }
-
-    companion object {
-        fun fromLine(line: String): PlayerRecord {
-            val rawPoints = line.split("|").drop(4).dropLast(1)
-
-            val nameAndSurname = line.split("|")[2].trim()
-            val points = rawPoints.map { it.trim().toIntOrNull() }
-
-            val result = PlayerRecord(nameAndSurname, points.toMutableList())
-            return result
-        }
-    }
-}
-
 fun main() {
     val path = "path/to/your/file.md"
 
